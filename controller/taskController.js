0658aa643a38e7ff603254e7dca2cc61b177fca1
ezequiel <ezequiel.zunesha@gmail.com>
2026-02-18 23:37:17 -0300

    feat: add correção de porta

--- controller/taskController.js
+++ controller/taskController.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+//URL da API para buscar os usuários
+//JSON Server está rodando na porta 3000, então a URL é essa.
 const URL = "http://localhost:3000/user";
 
 //Função para buscar os usuários da API
